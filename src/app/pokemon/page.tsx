7f8298a3fd6890aa3e5df689d238f0fb89e6a43d
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-10-17 23:19:06 +0900

    feat: ポストページとポケモンページの改善
    - ポストページのレイアウトを改善し、詳細リンクを追加
    - ポケモンページでポケモンのリストを表示する機能を追加
    - ナビゲーションコンポーネントのリンクを修正

--- src/app/pokemon/page.tsx
+++ src/app/pokemon/page.tsx
@@ -1,29 +1,54 @@
+import { DivideIcon } from "@heroicons/react/20/solid";
 import Image from "next/image";
+
+type Pokemons = {
+  next: string | null;
+  previous: string | null;
+  results: { name: string; url: string }[];
+};
 
 type Pokemon = {
   id: number;
   name: string;
-  sprites: {
-    front_default: string;
-  };
-  types: { type: { name: string } }[];
+  sprites: { other: { "official-artwork": { front_default: string } } };
 };
 
 export default async function Pokemon() {
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto`);
-  const pokemon: Pokemon = await res.json();
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon`);
+  const pokemons: Pokemons = await res.json();
+
+  const detailPokemons: Pokemon[] = await Promise.all(
+    pokemons.results.map(async (p) => {
+      const res = await fetch(p.url);
+      const data = await res.json();
+      return data;
+    }),
+  );
+
   return (
-    <div className="flex flex-grow items-center justify-center">
-      <div className="rounded-lg border border-gray-400 p-4">
-        <p className="text-center font-semibold">{pokemon.name}</p>
-        <Image
-          src={pokemon.sprites.front_default}
-          alt={pokemon.name}
-          width={150}
-          height={150}
-          className="mx-auto"
-        />
-      </div>
+    <div className="mx-auto my-16 max-w-7xl px-8">
+      <h2 className="text-3xl font-bold tracking-tight text-gray-900">
+        Pokemon Library
+      </h2>
+      <ul
+        role="list"
+        className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
+      >
+        {detailPokemons.map((pokemon) => (
+          <li key={pokemon.id}>
+            <div className="group relative overflow-hidden rounded-lg bg-gray-100 focus-within:outline-2 focus-within:outline-offset-2 focus-within:outline-indigo-600">
+              <Image
+                src={pokemon.sprites.other["official-artwork"].front_default}
+                alt="ポケモン画像"
+                width={500}
+                height={500}
+                className=" pointer-events-none rounded-lg object-cover outline -outline-offset-1 outline-black/5 group-hover:opacity-75"
+              />
+            </div>
+            <p className="font-semibold ">{pokemon.name}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/app/practice/page.tsx
+++ src/app/practice/page.tsx
@@ -5,32 +5,14 @@ import { ExclamationCircleIcon } from "@heroicons/react/16/solid";
 
 export default function Example() {
   return (
-    <div className="m-10 w-96">
-      <label
-        htmlFor="email"
-        className="block text-sm/6 font-medium text-gray-900"
-      >
-        Email
-      </label>
-      <div className="mt-2 grid grid-cols-1">
-        <input
-          defaultValue="adamwathan"
-          id="email"
-          name="email"
-          type="email"
-          placeholder="you@example.com"
-          aria-invalid="true"
-          aria-describedby="email-error"
-          className="col-start-1 row-start-1 block w-full rounded-md bg-white py-1.5 pr-10 pl-3 text-red-900 outline-1 -outline-offset-1 outline-red-300 placeholder:text-red-300 focus:outline-2 focus:-outline-offset-2 focus:outline-red-600 sm:pr-9 sm:text-sm/6"
-        />
-        <ExclamationCircleIcon
-          aria-hidden="true"
-          className="pointer-events-none col-start-1 row-start-1 mr-3 size-5 self-center justify-self-end text-red-500 sm:size-4"
-        />
+    <div className="mx-auto mt-10 flex max-w-7xl items-center px-8">
+      <div className="w-full border-t border-gray-900"></div>
+      <div className="relative flex justify-center">
+        <span className="bg-white px-3 text-base font-semibold text-gray-900">
+          Project
+        </span>
       </div>
-      <p id="email-error" className="mt-2 text-sm text-red-600">
-        Not a valid email address.
-      </p>
+      <div className="w-full border-t border-gray-300"></div>
     </div>
   );
 }
