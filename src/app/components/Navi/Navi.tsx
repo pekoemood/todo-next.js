b096a3257d73ec09eef91d9139856c67f50d71cb
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-10-26 23:32:07 +0900

    feat: ログインページを追加し、ナビゲーションにリンクを追加
    - ナビゲーションに「Login」リンクを追加
    - 新しいログインページコンポーネントを作成

--- src/app/components/Navi/Navi.tsx
+++ src/app/components/Navi/Navi.tsx
@@ -70,6 +70,9 @@ export default function Navi() {
               >
                 Practice
               </Link>
+              <Link href={"/login"} className={getNavLinkClasses("/login")}>
+                Login
+              </Link>
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
